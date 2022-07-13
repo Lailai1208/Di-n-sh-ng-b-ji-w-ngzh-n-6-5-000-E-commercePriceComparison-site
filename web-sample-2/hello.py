a1152fe970788f03d34182a4e5c7b458626c14fe
Ekko <w10036980@gmail.com>
2022-07-13 16:09:57 +0800

    fist

---
+++ web-sample-2/hello.py
@@ -0,0 +1,142 @@
+from datetime import datetime
+from sqlalchemy import create_engine
+import threading
+import crawler
+import pandas as pd
+from flask import Flask, request, render_template, redirect
+
+app = Flask(__name__)
+
+
+# =====
+# 呈現所有紀錄
+###
+# 1. 首頁可以看到所有「查詢記錄」
+# 2. 首頁可以啟動背景搜尋
+# 3. 每一筆記錄都看到爬蟲結果
+# =====
+
+
+conn = create_engine(
+    'mysql+mysqlconnector://root:lailai1208@127.0.0.1:3306/sample', echo=False)
+
+
+def yahoo(item, item_id):
+    print(" yahoo threading start: ", item)
+    d1 = crawler.yahoo(item)
+    d1['item'] = item_id
+    d1.to_sql(name='data', con=conn, if_exists='append', index=False)
+
+    with conn.connect() as con:
+        rs = con.execute(
+            f'UPDATE records SET status=status+1 WHERE id = {item_id};')
+
+    print(" ==== yahoo threading done ====")
+
+
+def momo(item, item_id):
+    print(" momo threading start: ", item)
+    d2 = crawler.momo(item)
+    d2['item'] = item_id
+    d2.to_sql(name='data', con=conn, if_exists='append', index=False)
+
+    with conn.connect() as con:
+        rs = con.execute(
+            f'UPDATE records SET status=status+1 WHERE id = {item_id};')
+
+    print(" ==== momo threading done ====")
+
+
+def pchome(item, item_id):
+    print(" pchome threading start: ", item)
+    d3 = crawler.pchome(item)
+    d3['item'] = item_id
+    d3['優惠'] = ''
+    d3.to_sql(name='data', con=conn, if_exists='append', index=False)
+
+    with conn.connect() as con:
+        rs = con.execute(
+            f'UPDATE records SET status=status+1 WHERE id = {item_id};')
+
+    print(" ==== pchome threading done ====")
+
+
+@app.route("/")
+def hello():
+
+    records = pd.read_sql(f'''SELECT *
+                            FROM records;
+                        ''', conn)
+
+    return render_template("hello.html",
+                           d=records.to_dict(orient='records'),
+                           )
+
+
+@app.route("/show/<item_id>")
+def show(item_id):
+
+    items = pd.read_sql(f'''SELECT *
+                            FROM records
+                            WHERE id = {item_id};
+                        ''', conn)
+
+    products = pd.read_sql(f'''SELECT *
+                            FROM data
+                            WHERE item = {item_id};
+                        ''', conn)
+
+    print(items)
+    return render_template("details.html",
+                           item=items.iloc[0, 1],
+                           d=products.to_dict(orient='records'),
+                           )
+
+
+@app.route("/search")
+def search():
+
+    item = request.args.get('item')
+    time = datetime.today()
+
+    if not item:
+        return render_template("none.html",
+                               item='',
+                               d=[],
+                               )
+
+    print(f'item => {item}')
+    print(f'time => {time}')
+
+    df = pd.DataFrame({
+        'item': [item],
+        'createdAt': [time],
+        'status': [0]
+    })
+
+    d = df.to_sql(name='records', con=conn, if_exists='append', index=False)
+    records = pd.read_sql(f'''SELECT *
+                            FROM records
+                            WHERE item = '{item}';
+                        ''', conn)
+
+    if len(df) == 0:
+        return render_template("none.html",
+                               item='',
+                               d=[],
+                               )
+
+    item_id = records.iloc[-1, 0]
+
+    t1 = threading.Thread(target=yahoo, args=(item, item_id,))
+    t1.start()
+    t2 = threading.Thread(target=momo, args=(item, item_id,))
+    t2.start()
+    t3 = threading.Thread(target=pchome, args=(item, item_id,))
+    t3.start()
+
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run()
